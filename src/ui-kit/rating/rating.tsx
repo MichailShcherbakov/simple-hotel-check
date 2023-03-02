606c762e47a7572af937a0211bca5789ca7b9bfd
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2023-03-02 18:09:00 +0300

    fix: add key prop

--- src/ui-kit/rating/rating.tsx
+++ src/ui-kit/rating/rating.tsx
@@ -11,13 +11,18 @@ export const UiRating: React.FC<UiRatingProps> = props => {
   const { value = 0, onChange } = props;
 
   function clickHandler(idx: number) {
+    if (value === idx + 1) {
+      return onChange?.(0);
+    }
+
     onChange?.(idx + 1);
   }
 
   return (
-    <div className="flex flex-row items-center gap-0.5">
+    <div className="flex flex-row items-center gap-0.5 select-none">
       {new Array(5).fill(0).map((_, idx) => (
         <StarIcon
+          key={idx}
           className={clsx("text-rad-400 cursor-pointer", {
             "text-yellow-400": value >= idx + 1,
             "text-yellow-300": value < idx + 1,
